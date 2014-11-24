97373c94d755a25be0c4441786d291323e94cd91
Guller Yuri <yuri.guller@hp.com>
2014-11-24 11:45:13 +0200

    initial commit

---
+++ src/test/java/com/hp/devops/demoapp/RestServletTest.java
@@ -0,0 +1,19 @@
+package com.hp.devops.demoapp;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: gullery
+ * Date: 23/11/14
+ * Time: 17:40
+ * To change this template use File | Settings | File Templates.
+ */
+public class RestServletTest {
+
+	@Test
+	public void unitTestA() {
+		Assert.assertEquals(true, true);
+	}
+}
