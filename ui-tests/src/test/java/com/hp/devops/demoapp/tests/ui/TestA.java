1940db2881ed0be6df581f6fad0a95feea8f62b2
Guller Yuri <yuri.guller@hp.com>
2014-11-25 17:55:53 +0200

    initial for selenuim tests

---
+++ ui-tests/src/test/java/com/hp/devops/demoapp/tests/ui/TestA.java
@@ -0,0 +1,20 @@
+package com.hp.devops.demoapp.tests.ui;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: gullery
+ * Date: 25/11/14
+ * Time: 17:28
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestA {
+
+	@Test
+	public void testUIcaseA() {
+
+		Assert.assertEquals(true, true);
+	}
+}
