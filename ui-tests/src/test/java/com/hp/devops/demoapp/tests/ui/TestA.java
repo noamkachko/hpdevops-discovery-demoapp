82ba4be4c6eefc9058178884e5fa6efec2e8fcc2
Guller Yuri <yuri.guller@hp.com>
2014-11-25 21:58:10 +0200

    added ui test

--- ui-tests/src/test/java/com/hp/devops/demoapp/tests/ui/TestA.java
+++ ui-tests/src/test/java/com/hp/devops/demoapp/tests/ui/TestA.java
@@ -1,7 +1,11 @@
 package com.hp.devops.demoapp.tests.ui;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.*;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 /**
  * Created with IntelliJ IDEA.
@@ -12,9 +16,23 @@ import org.junit.Test;
  */
 public class TestA {
 
+	static private WebDriver driver;
+
+	@BeforeClass
+	static public void beforeAll() {
+		driver = new HtmlUnitDriver();
+		driver.get("http://54.146.140.70:9000");
+	}
+
 	@Test
 	public void testUIcaseA() {
+		WebElement query = driver.findElement(By.id("bandsList"));
+		Assert.assertEquals(query.getTagName(), "div");
+		Assert.assertEquals(query.isDisplayed(), true);
+	}
 
-		Assert.assertEquals(true, true);
+	@AfterClass
+	static public void afterAll() {
+		driver.quit();
 	}
 }
