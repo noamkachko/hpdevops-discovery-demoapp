ecc0e451a3606a3e9fdcf0984a639257eb2bde66
Guller Yuri <yuri.guller@hp.com>
2014-12-03 17:07:29 +0200

    adding preps for the testng run

---
+++ ui-tests-testng/src/test/java/com/hp/devops/demoapp/tests/ui/TestSuiteA.java
@@ -0,0 +1,29 @@
+package com.hp.devops.demoapp.tests.ui;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: gullery
+ * Date: 03/12/14
+ * Time: 15:50
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestSuiteA {
+
+	@Test(groups = { "Group_A", "Group_1" })
+	public void testCase1() {
+
+	}
+
+	@Test
+	public void testCase2() {
+
+	}
+
+	@Test(groups = { "Group_A" })
+	public void testCase3() {
+
+	}
+
+}
---
+++ ui-tests-testng/src/test/java/com/hp/devops/demoapp/tests/ui/TestSuiteB.java
@@ -0,0 +1,29 @@
+package com.hp.devops.demoapp.tests.ui;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: gullery
+ * Date: 03/12/14
+ * Time: 15:50
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestSuiteB {
+
+	@Test(groups = { "Group_B", "Group_1" })
+	public void testCaseA() {
+
+	}
+
+	@Test
+	public void testCaseB() {
+
+	}
+
+	@Test(groups = { "Group_B" })
+	public void testCaseC() {
+
+	}
+
+}
