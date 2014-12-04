6984a0506eb523a93eb05b77dc02594a9d027e90
Boris Belozovsky <boris.belozovsky@hp.com>
2014-12-04 11:15:41 +0200

    Better handling of the parameters

--- ui-tests/src/test/java/com/hp/devops/demoapp/tests/ui/TestA.java
+++ ui-tests/src/test/java/com/hp/devops/demoapp/tests/ui/TestA.java
@@ -1,7 +1,9 @@
 package com.hp.devops.demoapp.tests.ui;
 
-import org.junit.*;
-
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
@@ -19,60 +21,63 @@ import org.openqa.selenium.remote.DesiredCapabilities;
  */
 public class TestA {
 
-	static private WebDriver driver;
-	static private boolean isBehindProxy = false;
-	static private String testProxy = "web-proxy.bbn.hp.com:8081";
+    static private WebDriver driver;
+    static private boolean isBehindProxy = false;
+    static private String testProxy = "web-proxy.bbn.hp.com:8081";
     static private String appUrl = "http://54.146.140.70:9000";
 
-	@BeforeClass
-	static public void beforeAll() {
+    @BeforeClass
+    static public void beforeAll() {
 
-        if("true".equals(System.getProperty("proxy"))){
+        if ("true".equals(System.getProperty("proxy"))) {
             isBehindProxy = true;
             System.out.println("isBehindProxy is true!");
-            if(System.getenv("testproxy")!=null){
+            if (System.getenv("testproxy") != null) {
                 testProxy = System.getenv("testproxy");
             }
             System.out.println("testProxy is " + testProxy + "; can be modified via environment variable, i.e., 'export textproxy=web-proxy.bbn.hp.com:8080'");
-        } else {
+        }
+        else {
             System.out.println("We do not use proxy");
         }
 
-		if (isBehindProxy) {
-			Proxy proxy = new Proxy();
-			proxy.setHttpProxy(testProxy);
-			DesiredCapabilities cap = new DesiredCapabilities();
-			cap.setCapability(CapabilityType.PROXY, proxy);
-			driver = new HtmlUnitDriver(cap);
-		} else {
-			driver = new HtmlUnitDriver();
-		}
-        if(System.getProperty("appUrl")!=null){
-            driver.get(System.getProperty("appUrl"));
-        } else {
-            driver.get(appUrl);
+        if (isBehindProxy) {
+            Proxy proxy = new Proxy();
+            proxy.setHttpProxy(testProxy);
+            DesiredCapabilities cap = new DesiredCapabilities();
+            cap.setCapability(CapabilityType.PROXY, proxy);
+            driver = new HtmlUnitDriver(cap);
         }
-        System.out.println("App URL is " + appUrl+"; can be modifed via system property, i.e., '-DappUrl=\"http://54.146.140.70:9000\"'");
+        else {
+            driver = new HtmlUnitDriver();
+        }
+        if (System.getProperty("appUrl") != null) {
+            appUrl = System.getProperty("appUrl");
+        }
+        System.out.println("App URL is " + appUrl + "; can be modifed via system property, i.e., '-DappUrl=\"http://54.146.140.70:9000\"'");
+
+        driver.get(appUrl);
+
     }
 
-	@Test
-	public void testUIcaseA() {
+    @Test
+    public void testUIcaseA() {
         System.out.println("Proudly running test " + Thread.currentThread().getStackTrace()[1]);
         WebElement query = driver.findElement(By.id("bandsList"));
-		Assert.assertEquals(query.getTagName(), "div");
-		Assert.assertEquals(query.isDisplayed(), true);
-	}
+        Assert.assertEquals(query.getTagName(), "div");
+        Assert.assertEquals(query.isDisplayed(), true);
+    }
 
-	@Test
-	public void testUIcaseB() {
+    @Test
+    public void testUIcaseB() {
         System.out.println("Proudly running test " + Thread.currentThread().getStackTrace()[1]);
-		WebElement query = driver.findElement(By.id("totalVotes"));
-		Assert.assertEquals(query.getTagName(), "div");
-		Assert.assertEquals(query.isDisplayed(), true);
-	}
+        WebElement query = driver.findElement(By.id("totalVotes"));
+        Assert.assertEquals(query.getTagName(), "div");
+        Assert.assertEquals(query.isDisplayed(), true);
+    }
 
-	@AfterClass
-	static public void afterAll() {
-		driver.quit();
-	}
+    @AfterClass
+    static public void afterAll() {
+        driver.quit();
+    }
 }
